de28f2d98dbc01949350a4781b90a1187c0d78a4
arpan2000-tes <216696451+arpan2000-tes@users.noreply.github.com>
2026-08-13 12:28:06 +0800

    add : new rotera sign in, sign up and auth

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -17,3 +17,18 @@ for user in userlist:
   print(user.name, user.email, user.password)
 
 return token
+
+@router.post("/signin")
+def sign_in(request: SignInSchema):
+   for user in userlist:
+      if user.email == request.email:
+         if user.password == request.password:
+            token = sign(user.email)
+            return token
+         else:
+            raise HTTPException(status_code=400, detail="incorrect password")
+   raise HTTPException(status_code=400, detail="emailnl not reqistered")
+   
+@router.post("/authtest")
+def auth_test(decode: str = Depends(decode)):
+   return decode
